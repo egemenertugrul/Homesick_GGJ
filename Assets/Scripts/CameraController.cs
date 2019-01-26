36fa57151596c3b3b73c7cd6fdf3e0575c42c307
圆小卿 <yuanxiaoqing@yuanxiaoqingdeMacBook-Pro.local>
2019-01-26 19:39:43 +0800

    CameraManager&&CameraController

---
+++ Assets/Scripts/CameraController.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour {
+
+    
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetMouseButton(1))
+        {
+            float x = Input.GetAxis("Mouse X");
+            float y = Input.GetAxis("Mouse Y");
+
+            transform.Rotate(Vector3.up,x);
+            transform.Rotate(Vector3.left, y);
+        }
+
+        float z = Input.GetAxis("Mouse ScrollWheel");
+        Debug.Log(z);
+        if (z>0.01f||z<-0.01f)
+        {
+            transform.Translate(Vector3.forward* Time.deltaTime, Space.World);
+        }
+    }
+}
