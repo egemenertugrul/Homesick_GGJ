7c1590877b7248fb91b060660bfb5ae8c291bf6f
oldwang-3gold <32608811+oldwang-3gold@users.noreply.github.com>
2019-01-26 19:23:03 +0800

    Add Car's Rotated &ViewChange&Sound

---
+++ Assets/Scripts/ViewFollow.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ViewFollow : MonoBehaviour
+{
+    public Vector3 set;
+
+    public Transform car;
+	// Use this for initialization
+	void Start ()
+	{
+	    set = transform.position - car.position;
+	}
+	
+	// Update is called once per frame
+	void Update ()
+	{
+	    transform.position = set + car.position;
+	}
+}
