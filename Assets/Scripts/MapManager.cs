c03ce922089ecffb9185813c4c71d272815d1257
egemenertugrul <egemenertugrul@yandex.com>
2019-01-26 18:55:25 +0800

    Added meteor map

--- Assets/Scripts/MapManager.cs
+++ Assets/Scripts/MapManager.cs
@@ -3,36 +3,66 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class MapManager : MonoBehaviour {
+public class MapManager : MonoBehaviour
+{
+
+    public enum e_MapType { SolarSystem, Asteroid }
+    public bool assignRandomTimeScales = false;
+    public e_MapType mapType;
 
     public float universalTimeScale = 1;
 
     KeplerOrbitMover[] keplerMovers;
 
     public GameObject[] earthSimplePlanetPrefabs;
+    public GameObject[] asteroidPrefabs;
 
-	// Use this for initialization
-	void Start () {
+
+    // Use this for initialization
+    void Start()
+    {
         keplerMovers = GetComponentsInChildren<KeplerOrbitMover>();
-        foreach(KeplerOrbitMover mover in keplerMovers)
-        {
-            MeshRenderer mr = mover.gameObject.GetComponent<MeshRenderer>();
-            if(mr) Destroy(mr);
-            MeshFilter mf = mover.gameObject.GetComponent<MeshFilter>();
-            if(mf) Destroy(mf);
-            int randomIndex = Random.Range(0, earthSimplePlanetPrefabs.Length);
-
-            GameObject newPlanet = Instantiate(earthSimplePlanetPrefabs[randomIndex], mover.transform);
-            float sphereScale = mover.transform.localScale.x; // Assuming x,y,z scales are the same
-            newPlanet.transform.localScale = new Vector3(sphereScale, sphereScale, sphereScale);
-        }
-	}
-	
-	// Update is called once per frame
-	void Update () {
         foreach (KeplerOrbitMover mover in keplerMovers)
         {
-            mover.TimeScale = universalTimeScale;
+            MeshRenderer mr = mover.gameObject.GetComponent<MeshRenderer>();
+            if (mr) Destroy(mr);
+            MeshFilter mf = mover.gameObject.GetComponent<MeshFilter>();
+            if (mf) Destroy(mf);
+
+            if (mapType == e_MapType.SolarSystem && earthSimplePlanetPrefabs.Length > 0)
+            {
+                int randomIndex = Random.Range(0, earthSimplePlanetPrefabs.Length);
+                GameObject newPlanet = Instantiate(earthSimplePlanetPrefabs[randomIndex], mover.transform);
+                float sphereScale = mover.transform.localScale.x; // Assuming x,y,z scales are the same
+                newPlanet.transform.localScale = new Vector3(sphereScale, sphereScale, sphereScale);
+            } else if(mapType == e_MapType.Asteroid && asteroidPrefabs.Length > 0)
+            {
+                int randomIndex = Random.Range(0, asteroidPrefabs.Length);
+                GameObject newAsteroid = Instantiate(asteroidPrefabs[randomIndex], mover.transform);
+                //float sphereScale = mover.transform.localScale.x; // Assuming x,y,z scales are the same
+                newAsteroid.transform.localScale = new Vector3(newAsteroid.transform.localScale.x * 5, newAsteroid.transform.localScale.y * 5, newAsteroid.transform.localScale.z * 5);
+                int randomRotX = Random.Range(0, 360);
+                int randomRotY = Random.Range(0, 360);
+                int randomRotZ = Random.Range(0, 360);
+                newAsteroid.transform.eulerAngles = new Vector3(randomRotX, randomRotY, randomRotZ);
+                if (assignRandomTimeScales)
+                {
+                    float randomTimeScale = Random.Range(1.0f, 500.0f);
+                    mover.TimeScale = randomTimeScale;
+                }
+            }
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (!assignRandomTimeScales)
+        {
+            foreach (KeplerOrbitMover mover in keplerMovers)
+            {
+                mover.TimeScale = universalTimeScale;
+            }
         }
     }
 }
---
+++ Assets/Store/Black Hole/Script/Demo.cs
@@ -0,0 +1,91 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Demo : MonoBehaviour
+{
+    public Material m_Mat = null;
+    [Range(0.01f, 0.2f)]
+    public float m_DarkRange = 0.1f;
+    [Range(-2.5f, -1f)]
+    public float m_Distortion = -2f;
+    [Range(0.05f, 0.3f)]
+    public float m_Form = 0.2f;
+    private float m_MouseX = 0f;
+    private float m_MouseY = 0f;
+    private bool m_TraceMouse = false;
+
+    Camera cam;
+
+    private bool blackHoleInRange = true;
+    public GameObject blackHole;
+
+    void Start()
+    {
+        cam = GetComponent<Camera>();
+
+        if (!SystemInfo.supportsImageEffects)
+            enabled = false;
+        m_MouseX = m_MouseY = 0.5f;
+    }
+    void OnRenderImage(RenderTexture sourceTexture, RenderTexture destTexture)
+    {
+        if (blackHoleInRange)
+        {
+            Vector3 coord = cam.WorldToScreenPoint(blackHole.transform.position);
+            float dist = Vector3.Distance(cam.transform.position, blackHole.transform.position);
+
+            m_Mat.SetVector("_Center", new Vector4(coord.x / Screen.width, coord.y / Screen.height, 0f, 0f));
+            //m_Mat.SetVector ("_Center", new Vector4 (m_MouseX, m_MouseY, 0f, 0f));
+            m_DarkRange = dist / 50 * 0.2f;
+            m_Mat.SetFloat("_DarkRange", m_DarkRange); // 0.1, (0.01)-(0.20)
+            m_Distortion = -(dist / 50 * 1.5f + 1);
+            m_Mat.SetFloat("_Distortion", m_Distortion); // -2, (-2.5)-(-1)
+            m_Mat.SetFloat("_Form", m_Form);
+            Graphics.Blit(sourceTexture, destTexture, m_Mat);
+        }
+    }
+    void Update()
+    {
+        //		if (Input.GetMouseButtonDown (1))
+        //		{
+        //			m_TraceMouse = true;
+        //		}
+        //		else if (Input.GetMouseButtonUp (1))
+        //		{
+        //			m_TraceMouse = false;
+        //		}
+        //		else if (Input.GetMouseButton (1))
+        //		{
+        //			if (m_TraceMouse)
+        //			{
+        //				m_MouseX = Input.mousePosition.x / Screen.width;
+        //#if UNITY_5
+        //				m_MouseY = 1f - Input.mousePosition.y / Screen.height;
+        //#else
+        //				m_MouseY = Input.mousePosition.y / Screen.height;
+        //#endif
+        //			}
+        //		}
+    }
+    void OnGUI()
+    {
+        GUI.Box(new Rect(10, 10, 200, 25), "Black Hole Demo");
+    }
+
+    //private void OnTriggerEnter(Collider other)
+    //{
+    //    if (other.GetComponent<BlackHoleScript>())
+    //    {
+    //        blackHoleInRange = true;
+    //        blackHole = other.gameObject;
+    //    }
+    //}
+
+    //private void OnTriggerExit(Collider other)
+    //{
+    //    if (other.GetComponent<BlackHoleScript>())
+    //    {
+    //        blackHoleInRange = false;
+    //    }
+    //}
+}
